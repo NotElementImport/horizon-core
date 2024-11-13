26d43af9e619d10c3afeeff91c85f8722cb14b50
P. Kirill <k.panteleev@ddirection.kz>
2024-11-13 09:49:00 +0500

    update local.test.mjs

--- local.test.mjs
+++ local.test.mjs
@@ -1,21 +1,22 @@
-import { defineApp } from "./bundle/app.mjs";
-import { comp } from "./bundle/component.mjs";
-import { useSignal } from "./bundle/stateble.mjs";
+// import { defineApp } from "./bundle/app.mjs";
+// import { comp } from "./bundle/component.mjs";
+// import { useSignal } from "./bundle/stateble.mjs";
 
-const useWait = async (ms = 1000) => {
-    return new Promise((resolve) => {
-        setTimeout(() => resolve(), ms)
-    })
-}
+// const useWait = async (ms = 1000) => {
+//     return new Promise((resolve) => {
+//         setTimeout(() => resolve(), ms)
+//     })
+// }
 
-const app = defineApp()
+// const app = defineApp()
 
-const articles = useSignal([
-    { title: 'Title 1' },
-    { title: 'Title 2' }
-], { bus: 'articles' })
+// const articles = useSignal([
+//     { title: 'Title 1' },
+//     { title: 'Title 2' }
+// ], { bus: 'articles' })
 
-const main = comp((_, { img }) => {
-})
+// const main = comp((_, { img }) => {
 
-await app.renderSSR(main, { withMeta: true, unmountAtEnd: true })
+// })
+
+// await app.renderSSR(main, { withMeta: true, unmountAtEnd: true })
